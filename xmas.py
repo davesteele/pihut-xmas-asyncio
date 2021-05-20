5a4a8d7c8a4db7162813148706b6a03e7e0020ec
David Steele <steele@debian.org>
2021-05-19 22:07:50 -0400

    Clean up example

--- xmas.py
+++ xmas.py
@@ -21,11 +21,6 @@ def calcplusorminus(val, var):
     return factor * val
 
 
-def do_sigterm():
-    """SIGTERM triggers the KeyboardInterrupt handler."""
-    raise KeyboardInterrupt
-
-
 async def blink_led(ledno):
     """The led blinking coroutine for one led."""
     ontime = calcplusorminus(ONTIME, PLUSORMINUS)
@@ -44,18 +39,36 @@ async def blink_led(ledno):
         GPIO.setup(ledno, GPIO.IN)
 
 
-# create a Task for each led.
-# gpio 2 is the yellow star - 4-27 are the other tree lights
-tasks = [asyncio.ensure_future(blink_led(x)) for x in range(2, 28) if x != 3]
+async def amain():
+    # create a Task for each led.
+    # gpio 2 is the yellow star - 4-27 are the other tree lights
+    loop = asyncio.get_event_loop()
+    tasks = [loop.create_task(blink_led(x)) for x in range(2, 28) if x != 3]
 
-loop = asyncio.get_event_loop()
-loop.add_signal_handler(signal.SIGTERM, do_sigterm)
+    try:
+        await asyncio.gather(*tasks)
+    except asyncio.CancelledError:
+        [task.cancel() for task in tasks]
+        await asyncio.gather(*tasks)
 
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    aggregate = asyncio.gather(*tasks)
-    aggregate.cancel()
-    loop.run_until_complete(aggregate)
 
-loop.close()
+def do_sigterm():
+    """SIGTERM triggers the KeyboardInterrupt handler."""
+    raise KeyboardInterrupt
+
+
+def main():
+    loop = asyncio.get_event_loop()
+    loop.add_signal_handler(signal.SIGTERM, do_sigterm)
+
+    task = loop.create_task(amain())
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        task.cancel()
+        loop.run_until_complete(task)
+
+
+if __name__ == "__main__":
+    main()
